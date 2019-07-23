cbaab02a2ca0ccd31857e593d581b728a5e0fbba
Rob Brown <39971002+TubeTester@users.noreply.github.com>
2019-07-23 05:14:22 -0500

    Add files via upload

---
+++ helloworld/helloworld.py
@@ -0,0 +1,18 @@
+import discord
+from redbot.core import Config, commands, checks
+
+class helloworld_cog(getattr(commands, "Cog", object)):
+    """Hello World V3 cog!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx):
+        """This is a Hello World V3 cog!"""
+
+        #Your code will go here
+        await ctx.send("Hello World!")
+
+def setup(bot):
+    bot.add_cog(helloworld_cog(bot))
