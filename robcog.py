076778db22f281d95cd75e96e659f51b31f00fab
Rob Brown <39971002+TubeTester@users.noreply.github.com>
2019-07-23 01:54:45 -0500

    Add files via upload

--- robcog.py
+++ robcog.py
@@ -1,10 +1,18 @@
-from redbot.core import commands
+import discord
+from redbot.core import Config, commands, checks
 
-class Robcog(commands.Cog):
-    """Rob's custom cog"""
+class Robcog(getattr(commands, "Cog", object)):
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def rob(self):
         """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Robcog(bot))
