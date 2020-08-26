2eb2abc67e60da1b45ec115ff9c2c8e42bac4832
Rob Brown <39971002+TubeTester@users.noreply.github.com>
2020-08-25 23:37:15 -0500

    Update embedwiz.py

--- embedwiz/embedwiz.py
+++ embedwiz/embedwiz.py
@@ -240,7 +240,7 @@ class EmbedWizard(commands.Cog):
             return
 
         new_embed = await self._parse_embed(ctx, specification, force_author=not override)
-        await self.bot.edit_message(msg, embed=new_embed)
+        await channel.edit_message(msg, embed=new_embed)
         await ctx.send('Embed edited successfully.')
 
     def _check_override(self, ctx,  member):
