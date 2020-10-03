3eac813d468ecf7c8b7b7dca3a907da84ab68783
Rob Brown <39971002+TubeTester@users.noreply.github.com>
2020-10-03 00:16:01 -0500

    Update invitespamkiller.py

--- invite_spam_killer/invitespamkiller.py
+++ invite_spam_killer/invitespamkiller.py
@@ -75,10 +75,10 @@ class InviteSpamKiller(commands.Cog):
         #if ctx.cog is self:
         #msg = error("Created Invite.")
         #await ctx.send(msg)
-        channel = invite.channel 
+        #channel = invite.channel 
         #guild.get_channel(601966081711800355)        
-        if channel is not None:
-            await invite.user.send('Invite Created by {0}.'.format(member))
+        #if channel is not None:
+        await invite.user.send('Invite Created by {0}.'.format(member))
 
     @checks.mod_or_permissions(manage_messages=True)
     @commands.group(pass_context=True, invoke_without_command=True)
