c8ce362b364b220b13a76baf65f8edc36fdcf674
Rob Brown <39971002+TubeTester@users.noreply.github.com>
2025-01-04 20:48:15 -0600

    Update helloworld.py

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -18,9 +18,11 @@ class helloworld_cog(getattr(commands, "Cog", object)):
     async def list_members(self, ctx): 
         guild = ctx.guild
         members = []  
-        ### get_members = ([m for m in guild.members])  
-        ### for bot in get_members:  
-            ###members.append(m.name)  
+        get_members = ([member for member in guild.members if not member.bot])  
+        for member in get_members:  
+            members.append(member.name)
+            await ctx.send(f'{m}')
+            await ctx.send('DONE!')
         ###for member in guild.fetch_members(limit=150):
            ### print(member.name)
             
