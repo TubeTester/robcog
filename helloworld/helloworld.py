753ed59eece8982e149899cebfcdb9db61b8a9be
Rob Brown <39971002+TubeTester@users.noreply.github.com>
2025-01-04 21:51:18 -0600

    Update helloworld.py

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -22,9 +22,10 @@ class helloworld_cog(getattr(commands, "Cog", object)):
         get_members = ([member for member in guild.members if not member.bot])  
         for member in get_members:  
             async for message in channel.history(limit=200, oldest_first=True):
-                if message.author == member:
-                    
-            members.append(member.name + ',' + message.content + ' \r')
+                if message.author == member:                    
+                    members.append(member.name + ',' + message.content + ' \r')
+                else
+                    members.append(member.name + ', \r')
             ###last_message = [message async for message in channel.history(limit=1, oldest_first=True)]
             ###await ctx.send(f'{member}')
 
